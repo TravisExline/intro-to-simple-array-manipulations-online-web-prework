2481ed15513e8d09b7c438fcdf389b81123eec62
Travis Exline <t.exline22@gmail.com>
2019-04-30 18:33:15 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,47 @@
+def using_push(countries, country_string)
+  countries.push(country_string)
+end
+
+def using_unshift(city, city_string)
+  city.unshift(city_string)
+end
+
+def using_pop(songs)
+  songs.pop
+end
+
+def pop_with_args(thrones)
+  thrones.pop(2)
+end
+
+def using_shift(favorite_cities)
+  favorite_cities.shift
+end
+
+def shift_with_args(ice_cream)
+  ice_cream.shift(2)
+end
+
+def using_concat(favorites, more_favorites)
+  favorites.concat(more_favorites)
+end
+
+def using_insert(languages, another_one)
+  languages.insert(4, another_one)
+end
+
+def using_uniq(planeteers)
+  planeteers.uniq
+end
+
+def using_flatten(private_colleges)
+  private_colleges.flatten
+end
+
+def using_delete(instructors, name)
+  instructors.delete(name)
+end
+
+def using_delete_at(robots, bad_robot)
+  robots.delete_at(bad_robot)
+end
