3b723d3977bad3cf5500f09651efaac26bb1eba6
Travis Exline <t.exline22@gmail.com>
2019-04-30 17:59:13 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,23 @@
+def using_push(countries, country_string)
+  countries.push(country_string)
+end
+
+def using_unshift(city, city_string)
+  city.unshift(city_string)
+end
+
+def using_pop(songs)
+  songs.pop
+end
+
+def pop_with_args(thrones)
+  thrones.pop(2)
+end
+
+def using_shift(favorite_cities)
+  favorite_cities.shift
+end
+
+def shift_with_args(ice_cream)
+  ice_cream.shift(2)
+end
